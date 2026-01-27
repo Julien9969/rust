988daa8893cabc03c7be6ba15f92db9dcdcca77a
Julien Roux <julien.roux404@gmail.com>
2026-01-27 20:20:52 +0100

    rename TemplateApp to ActivityTrackerApp across the project

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ fn main() -> eframe::Result {
     eframe::run_native(
         "eframe template",
         native_options,
-        Box::new(|cc| Ok(Box::new(activity_tracker::TemplateApp::new(cc)))),
+        Box::new(|cc| Ok(Box::new(activity_tracker::ActivityTrackerApp::new(cc)))),
     )
 }
 
@@ -50,7 +50,7 @@ fn main() {
             .start(
                 canvas,
                 web_options,
-                Box::new(|cc| Ok(Box::new(activity_tracker::TemplateApp::new(cc)))),
+                Box::new(|cc| Ok(Box::new(activity_tracker::ActivityTrackerApp::new(cc)))),
             )
             .await;
 
